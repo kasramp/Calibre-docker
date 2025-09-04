2535b9ac084cf35d2ecc1098577bd1ddfb839a3d
kasramp <kasra_mp@yahoo.com>
2025-09-04 14:04:27 +0000

    Add pipeline to build and push Docker image

---
+++ util/release.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -ev
+
+IMAGE_NAME=kasramp/calibre-docker
+TAGGED_VERSION=$(<../version.txt)
+
+docker login -u "$DOCKER_USERNAME" --password-stdin <<< "$DOCKER_PASSWORD" &&
+docker buildx create --use &&
+docker buildx build --pull --platform linux/amd64,linux/arm64 --build-arg VERSION="$TAGGED_VERSION" -t "$IMAGE_NAME":latest -t "$IMAGE_NAME:$TAGGED_VERSION" . --push
+echo "Successfully build and pushed Docker $TAGGED_VERSION to Docker Hub"
